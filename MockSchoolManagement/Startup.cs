22c04ef1902b29cc7f8f1fdbbd911f9a9736c6e8
Nightstars <christzhangowner#gmail.com>
2020-12-24 22:26:11 +0800

    添加项目文件。

---
+++ MockSchoolManagement/Startup.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MockSchoolManagement
+{
+    public class Startup
+    {
+        #region inisialize
+        private IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        #endregion
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                //防止乱码
+                context.Response.ContentType = "text/plain;charset=utf-8";
+                //注入后通过_configuration访问MyKey
+                await context.Response.WriteAsync(_configuration["MyKey"]);
+            });
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    var processname = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+                    await context.Response.WriteAsync(processname);
+                });
+            });
+        }
+    }
+}
